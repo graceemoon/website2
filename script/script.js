e2c22192e748d6ce819c8873221e69e0450455f2
grace moon <hi.gracemoon@gmail.com>
2017-06-08 04:54:28 -0400

    added 3rd column

--- script/script.js
+++ script/script.js
@@ -1,3 +1,38 @@
 console.log("gracee's website #2 dedicated to sexy makeup");
 
 "use strict";
+
+
+$('nav').hide();
+
+$('.burger').click(function() {
+	$('nav').slideToggle(280, function() {
+		// direction: 'left'
+		$('nav').css("z-index", "100");
+	})
+})
+
+
+var main = function() {
+  /* Push the body and the nav over by 285px over */
+  $('.icon-menu').click(function() {
+    $('.menu').animate({
+      left: "0px"
+    }, 200);
+
+    $('body').animate({
+      left: "285px"
+    }, 200);
+  });
+
+  /* Then push them back */
+  $('.icon-close').click(function() {
+    $('.menu').animate({
+      left: "-285px"
+    }, 200);
+
+    $('body').animate({
+      left: "0px"
+    }, 200);
+  });
+};
