75433a8ce840756e16a5eac09a2db74c55678e78
grace moon <hi.gracemoon@gmail.com>
2017-06-08 06:58:34 -0400

    added spans to all products

--- script/script.js
+++ script/script.js
@@ -7,37 +7,14 @@ $('nav').hide();
 
 $('.burger').click(function() {
 	$('nav').slideToggle(280, function() {
-		direction: 'left'
+		// direction: 'left'
 		$('nav').css("z-index", "100");
 	})
 })
 
 
-// var main = function() {
-//   $('.icon-menu').click(function() {
-//     $('.menu').animate({
-//       left: "0px"
-//     }, 200);
 
-//     $('body').animate({
-//       left: "285px"
-//     }, 200);
-//   });
-
-//   $('.icon-close').click(function() {
-//     $('.menu').animate({
-//       left: "-285px"
-//     }, 200);
-
-//     $('body').animate({
-//       left: "0px"
-//     }, 200);
-//   });
-// };
-
-
-
-// hover over image with .iz class for zoom
+// image hover zoom/rotation
     $('.product a').hover(function() {
         $('.iz').addClass('transition');
     }, function() {
